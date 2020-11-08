960d0eb67bfb9b7b4285d2268f53fc89553ed170
Java Software <pj.midgard@yahoo.com>
2020-11-08 13:15:49 +0000

    Add files via upload

---
+++ CA1groupk.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+
+We write software that has:
+Method 0: Authors
+Method 1: print ‘Hello World’
+Method 2: add two numbers
+Method 3: subtract one number from another
+Method 4: Multiply two numbers
+
+ */
+
+
+
+
+package ca1groupk;
+
+
+
+/**
+ *
+ * @author Angel2014fly
+ */
+public class CA1groupk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //I have created the CA1groupk main class in the program.
+        //I have created the myObj.x. in the program.
+        MyAuthors myObjl = new MyAuthors();
+        System.out.println(myObjl.x);
+        System.out.println(myObjl.x1);
+        System.out.println(myObjl.x2);
+        System.out.println(myObjl.x3);
+        System.out.println(myObjl.x4);
+        System.out.println(myObjl.x5);
+        System.out.println(myObjl.x6);
+        System.out.println(myObjl.x7);
+    }
+    
+    
+}
