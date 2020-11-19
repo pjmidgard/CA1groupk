07cda471d520b2eb997736b9434ed0c243de0ed3
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2020-11-19 21:51:50 +0000

    Update method.java

--- code/method.java
+++ code/method.java
@@ -16,3 +16,4 @@ public static void main(String[] args) {
     BufferedReader someInputVar = new BufferedReader(new InputStreamReader(System.in));
          String myName = "Luiza";
                System.out.println ("Hello world by: " + myName); }}
+               
