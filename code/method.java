9ea51623ea445fa998e0f67c8c50b365a62156d4
luizaalbuquerque <izaalbuquerque10@hotmail.com>
2020-11-19 21:16:11 +0000

    Create method.java

---
+++ code/method.java
@@ -0,0 +1,18 @@
+package myfirstproject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author luizaalbuquerque
+ */
+public class MyFirstProject {
+
+    /**
+ * @param args the command line arguments
+     */
+public static void main(String[] args) {
+    BufferedReader someInputVar = new BufferedReader(new InputStreamReader(System.in));
+         String myName = "Luiza";
+               System.out.println ("Hello world by: " + myName); }}
