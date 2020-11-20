355b2b45a158c83ff81c1b409a4f331d287468c6
Java Software <pj.midgard@yahoo.com>
2020-11-20 19:52:31 +0000

    Add files via upload

---
+++ helloword/Helloword.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+/**
+ *
+ * @author Angel2014fly
+ */
+public class Helloword {
+
+    /**
+     * @param args the command line arguments
+     */
+     static void HW(){ 
+        
+               System.out.println ("Hello World! - Andre Goncalves, Brazil\", \"Hello World! Lucas Gustavo, Brazil"); 
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HW();
+    }
+    
+}
