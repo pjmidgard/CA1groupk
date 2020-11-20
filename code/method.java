8eb0fc3dfcbb1b4355db2537a1556acde9c1afb8
edgardpacheco007 <72029558+edgardpacheco007@users.noreply.github.com>
2020-11-20 13:04:54 +0000

    Update method.java
    Follow my code

--- code/method.java
+++ code/method.java
@@ -3,6 +3,8 @@ package myfirstproject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 /**
  *
  * @author luizaalbuquerque
@@ -12,8 +14,39 @@ public class MyFirstProject {
     /**
  * @param args the command line arguments
      */
-public static void main(String[] args) {
+    
+    
+     public static void main(String[] args) {
+
+    MultiplyTwoNumbers();
+
     BufferedReader someInputVar = new BufferedReader(new InputStreamReader(System.in));
          String myName = "Luiza";
-               System.out.println ("Hello world by: " + myName); }}
+               System.out.println ("Hello world by: " + myName); 
+
+
+            }
+            
+            
+/** 
+ * @author EdgardPacheco
+ */     
+            
+/** 
+ * @author EdgardPacheco
+ */     
+            
+        static void MultiplyTwoNumbers() {
+            int n1 = 5;
+            int n2 = 6;
+            int r = n2 * n2;
+
+            System.out.println(n1 +"x" + n2 + " = " + r + " by: Edgard Pacheco");
+        }
+            
+            
+            
+            
+            
+            }
                
