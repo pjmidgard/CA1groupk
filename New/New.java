6758e135e3032ee94ff8256c097d8ed4aeab8534
Java Software <pj.midgard@yahoo.com>
2020-11-20 14:30:47 +0000

    Add files via upload

--- New/New.java
+++ New/New.java
@@ -1,19 +1,27 @@
-package myfirstproject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgnew;
 
 /**
  *
- * @author luizaalbuquerque
+ * @author Angel2014fly
  */
-public class MyFirstProject {
+public class New {
 
     /**
- * @param args the command line arguments
+     * @param args the command line arguments
      */
-public static void main(String[] args) {
-    BufferedReader someInputVar = new BufferedReader(new InputStreamReader(System.in));
-         String myName = "Luiza";
-               System.out.println ("Hello," + myName); }}
-              
+    static void HW() {
+    String myName = "Luiza";
+               System.out.println ("Hello," + myName); 
+}
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HW();
+    }
+    
+}
