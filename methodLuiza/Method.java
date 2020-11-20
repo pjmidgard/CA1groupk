a4287b21d28c1289c87c9bfad84f8da2b1f41919
Java Software <pj.midgard@yahoo.com>
2020-11-20 18:42:15 +0000

    Update Method.java

--- methodLuiza/Method.java
+++ methodLuiza/Method.java
@@ -1,5 +1,4 @@
-
-            /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -19,14 +18,18 @@ public class Method {
     public static void main(String[] args) {
         // TODO code application logic here
         MultiplyTwoNumbers();
-
+        HW();
    
-         String myName = "Luiza";
-               System.out.println ("Hello world by: " + myName); 
+         
 
 
     
 }
+    
+    static void HW(){
+        String myName = "Luiza";
+               System.out.println ("Hello world by: " + myName); 
+    }
        static void MultiplyTwoNumbers() {
             int n1 = 5;
             int n2 = 6;
